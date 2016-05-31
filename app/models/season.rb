1314e3261856018ad70f8b291e6dc27bda95c260
chibinaoko <tranghuyenttht@gmail.com>
2016-05-31 09:08:49 +0700

    view posts

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,8 @@
 class Season < ActiveRecord::Base
   has_many :season_teams
   has_many :matches, dependent: :destroy
-  has_many :posts
+  has_many :season_teams, dependent: :destroy
+  has_many :posts, dependent: :destroy
   has_many :team, through: :season_teams
 
   validates :name, presence: true, length: {maximum: 50}
