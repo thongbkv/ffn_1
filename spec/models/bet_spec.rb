3b925b630e25bd920988f8a33cece3f648a3c84b
thongbkv <framgia@framgia0160-lt.framgia.com>
2016-06-08 14:05:52 +0700

    bet-rspec

--- spec/models/bet_spec.rb
+++ spec/models/bet_spec.rb
@@ -3,7 +3,7 @@ require "rspec/mocks"
 
 RSpec.describe Bet, type: :model do
   let(:bet) {FactoryGirl.create :bet}
-
+  subject{bet}
   describe "Associations" do
     it "should belongs_to User" do
       t = Bet.reflect_on_association(:user)
@@ -14,6 +14,18 @@ RSpec.describe Bet, type: :model do
       t = Bet.reflect_on_association(:match)
       t.macro.should eq :belongs_to
     end
+  end
 
+  describe "Bet" do
+    before {FactoryGirl.create :user}
+    it {is_expected.to callback(:send_mail).after(:save) }
+  end
+
+  describe "#is_win_of_team?" do
+    let(:win_team_id) {Bet.find_by team_id: 1}
+    let(:different_win_team_id) {Bet.find_by team_id: 2
+
+    it {expect(subject.is_win_of_team? 1).to eq true}
+    it {expect(subject.is_win_of_team? 2).to eq false}
   end
 end
